2c2323d4abea9230331262da47fe30822d3f95c4
Swathi Shenoy M <swathishenoyms96@gmail.com>
2019-10-21 22:04:41 +0530

    generated 3 components

--- practiceone/src/app/app.module.ts
+++ practiceone/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
